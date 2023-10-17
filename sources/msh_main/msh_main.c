6295cf0029d2ee34f899ba74eab1f03f5c499057
Haeseong Kang <gotjd7837@gmail.com>
2023-10-17 16:43:37 +0900

    main

--- sources/msh_main/msh_main.c
+++ sources/msh_main/msh_main.c
@@ -6,7 +6,7 @@
 /*   By: haekang <haekang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 12:03:25 by jho               #+#    #+#             */
-/*   Updated: 2023/10/16 22:15:27 by haekang          ###   ########.fr       */
+/*   Updated: 2023/10/16 22:21:40 by haekang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ int	main(int argc, char *argv[], char *envp[])
 {
 	atexit(leaks);
 	t_env	*env;
-	char	*input;
-	t_token	*tokens;
+	// char	*input;
+	// t_token	*tokens;
 
 	(void) argc;
 	(void) argv;
@@ -32,21 +32,21 @@ int	main(int argc, char *argv[], char *envp[])
 		write(2, "msh : failed to read environment.\n", 34);
 		return (1);
 	}
-	// char **cmd;
-	// cmd = malloc(sizeof(char *) * 4);
-	// cmd[0] = "echo";
-	// cmd[1] = "fuck";
-	// cmd[2] = "you";
-	// cmd[3] = "man";
-	// msh_echo(cmd);
-	// free(cmd);
-	// exit(0);
-	while (1)
-	{
-		input = readline("msh$> ");
-		tokens = msh_lexical_analysis(input, env);
-		free(input);
-		exit(0);
-	}
+	char **cmd;
+	cmd = malloc(sizeof(char *) * 4);
+	cmd[0] = "echo";
+	cmd[1] = "fuck";
+	cmd[2] = "you";
+	cmd[3] = "man";
+	msh_echo(cmd);
+	free(cmd);
+	exit(0);
+	// while (1)
+	// {
+	// 	input = readline("msh$> ");
+	// 	tokens = msh_lexical_analysis(input, env);
+	// 	free(input);
+	// 	exit(0);
+	// }
 	return (0);
 }
